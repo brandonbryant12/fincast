f98bbbd9a7061d1c7070d79d087083839f79dd6e
Brandon Bryant <brandon.bryant002@gmail.com>
2025-04-03 19:22:50 -0400

    add config

--- apps/fincast-client/tailwind.config.js
+++ apps/fincast-client/tailwind.config.js
@@ -1,14 +1,15 @@
-// tailwind.config.mjs
-import { defineConfig } from 'tailwindcss'
+import { defineConfig } from 'tailwindcss';
+import uiTailwindConfig from '../../packages/ui/tailwind.config.js';
 
 export default defineConfig({
+  presets: [uiTailwindConfig],
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-})
+});
